0dcc331b270d48eaadb2b8fc00265b1d5e7d0f44
Stefan B <Stefan.bie@live.de>
2016-01-13 16:58:57 +0100

    - renamed to bundleLoader

--- addon/mixins/lazy-route.js
+++ addon/mixins/lazy-route.js
@@ -5,11 +5,11 @@ import Ember from "ember";
 
 export default Ember.Mixin.create({
 
-    bundles: Ember.inject.service(),
+    bundleLoader: Ember.inject.service("bundle-loader"),
 
     beforeModel: function(transition, queryParams){
         this._super(transition,queryParams);
-        let service = this.get("bundle-loader");
+        let service = this.get("bundleLoader");
         return service.loadBundle(transition.targetName);
     }
 
