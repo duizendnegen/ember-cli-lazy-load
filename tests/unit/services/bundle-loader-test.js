3f2834caf15e7a1e03cefbd28d3fbdc69d638a71
Stefan B <Stefan.bie@live.de>
2016-01-13 17:18:22 +0100

    - add tests for the internal functions

--- tests/unit/services/bundle-loader-test.js
+++ tests/unit/services/bundle-loader-test.js
@@ -1,5 +1,6 @@
 /* jshint expr:true */
 import { expect } from 'chai';
+import Ember from "ember";
 import {
   describeModule,
   it
@@ -7,7 +8,7 @@ import {
 
 describeModule(
   'service:bundle-loader',
-  'BundlesService',
+  'Bundle-Loader-Service',
   {
     // Specify the other units that are required for this test.
      needs: ['service:bundle-loader']
@@ -19,10 +20,23 @@ describeModule(
       expect(service).to.be.ok;
     });
 
-    it('has public mehtod loadBundle', function() {
+    it('has public method loadBundle', function() {
       let service = this.subject();
       expect(service).to.respondTo("loadBundle");
     });
 
+    it('intern functions works', function() {
+      let service = this.subject();
+
+      service.getScript = function(url){
+          return Ember.RSVP.Promise((resolve /*,reject*/)=>{
+            setTimeout(()=>resolve(url),1000);
+          });
+      };
+      service.loadBundle("index").then((url)=>{
+        expect(url[0]).equals["index"];
+      });
+
+    });
   }
 );
