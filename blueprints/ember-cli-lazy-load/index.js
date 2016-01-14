9dcd494fdb3c1e927d72bae6c06a17412c320559
Stefan B <Stefan.bie@live.de>
2016-01-14 12:24:20 +0100

    fix entry name

--- blueprints/ember-cli-lazy-load/index.js
+++ blueprints/ember-cli-lazy-load/index.js
@@ -2,4 +2,13 @@
 
 module.exports = {
     description: 'Generates a placeholder bundle config.'
+
+    /**
+     * Default blueprint don't need an entry name
+     * @param entityName
+     * @returns {string}
+     */
+    normalizeEntityName: function(entityName) {
+        return ""
+    },
 };
